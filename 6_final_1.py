ead04d8d1d7eb73bc5ab94ab45039847bb8e0259
seulgiroom <sssggg1ee@gmail.com>
2022-05-14 10:39:22 +0900

    20220514

---
+++ 6_final_1.py
@@ -0,0 +1,7 @@
+input_id = input('id:')
+input_pw = input('pw:')
+
+if input_id == 'egoing' and input_pw == '1111':
+    print('Welcome')
+else input_id == 'basta' and input_pw == '2222':
+    print('Welcome')
---
+++ 6_final_2.py
@@ -0,0 +1,5 @@
+input_id = input('id:')
+input_pw = input('pw:')
+
+if input_id == 'egoing' or input_id == 'basta':
+    print('welcome')
